a4253ad6914e46439fb4b370df6911835ea78ff8
PritiKedar <pritikedar@gmail.com>
2022-06-21 22:29:26 -0400

    homepage styling

---
+++ src/main/java/com/pritikedar/chocolateria/model/Cart.java
@@ -0,0 +1,51 @@
+//package com.pritikedar.chocolateria.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name ="cart")
+//public class Cart {
+//	private long id;
+//	private String name;
+//	private double price;
+//	private int quantity;
+//	private double subtotal;
+//	
+//	
+//	public long getId() {
+//		return id;
+//	}
+//	public void setId(long id) {
+//		this.id = id;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public double getPrice() {
+//		return price;
+//	}
+//	public void setPrice(double price) {
+//		this.price = price;
+//	}
+//	public int getQuantity() {
+//		return quantity;
+//	}
+//	public void setQuantity(int quantity) {
+//		this.quantity = quantity;
+//	}
+//	
+//	public double subtotal() {
+//		subtotal = price*quantity;
+//		
+//		return subtotal ;
+//		
+//	
+//	}
+//	
+//	
+//
+//}
